99a2ba636376185b03a8f50259c1d42a01b4934a
Daniil Klementiev <klementevdaniil@gmail.com>
2024-01-11 18:19:01 +0200

    db show tables hw

--- cgi/index.py
+++ cgi/index.py
@@ -9,24 +9,48 @@ print() # end of headers
 with open('home.html', encoding="utf-8") as file :
     print( file.read() )
 
-import cgi
-
+# import cgi
+# import MySQLdb
+    
 # print("Content-type: text/html\n")
 
-# # Забезпечення відображення обраних змінних оточення
 # selected_env_vars = ["REQUEST_URI", "QUERY_STRING", "REQUEST_METHOD", "REMOTE_ADDR", "REQUEST_SCHEME"]
 # for var_name in selected_env_vars:
 #     print(f"{var_name}: {os.environ.get(var_name, '')}<br>")
 
 # print("<br>")
 
-# # Розбір рядка QUERY_STRING у словник
 # query_string = os.environ.get("QUERY_STRING", "")
 # query_dict = cgi.parse_qs(query_string)
 
-# # Виведення результатів розбору
 # print("Query string dictionary:<br>")
 # print("{<br>")
 # for key, values in query_dict.items():
 #     print(f'  "{key}": {values},<br>')
 # print("}<br>")
+    
+
+# db_host = "your_db_host"
+# db_user = "your_db_user"
+# db_password = "your_db_password"
+
+
+# db = MySQLdb.connect(host=db_host, user=db_user, passwd=db_password)
+# cursor = db.cursor()
+
+# sql_query = "SHOW DATABASES"
+# cursor.execute(sql_query)
+# databases = cursor.fetchall()
+
+# print("<html><head><title>Database List</title></head><body>")
+# print("<h1>Database List:</h1>")
+# print("<table border='1'>")
+# print("<tr><th>Database Name</th></tr>")
+
+# for database in databases:
+#     print(f"<tr><td>{database[0]}</td></tr>")
+
+# print("</table></body></html>")
+
+# cursor.close()
+# db.close()
